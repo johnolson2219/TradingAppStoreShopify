8a458a7377f8501c896de594c9acae1f89bb82c7
kakashitheaf <kakashi.theaf0109@gmail.com>
2023-07-12 17:18:50 -0400

    test Tradingapp

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-const crypto = require("crypto");
-const nonce = require("nonce");
-const request = require("request-promise");
-const querystring = require("querystring");
-const cookie = require("cookie");
-const express = require("express");
-
-const app = express(); // initialize application
-app.listen(3434, () => console.log("Application listening on port 3434!"));
