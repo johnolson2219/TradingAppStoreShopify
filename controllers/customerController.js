c63867da03c7106d56f42938675811992bdfe661
kakashitheaf <kakashi.theaf0109@gmail.com>
2023-07-19 11:18:36 -0400

    getorders5

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -153,7 +153,7 @@ exports.getWebhook = async (req, res, next) => {
       // Get the orderId
       // const orders = await axios.get('https://app.digital-downloads.com/api/v1/orders').then((r) => r.data);
       let myorders;
-      const apiOrderURL = `https://${shopifyDomain}/admin/order.json`;
+      const apiOrderURL = `https://${shopifyDomain}/admin/orders.json`;
       const apiOrderHeaders = {
         "X-Shopify-Access-Token": accessToken,
       };
